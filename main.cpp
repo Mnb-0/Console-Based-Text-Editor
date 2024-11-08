197a54beccfd4443b61e7cfe7a158a647afb3874
Muneeb <150353438+Mnb-0@users.noreply.github.com>
2024-11-08 20:40:04 +0500

    Created Stack Class
    Created a basic StackNode class and Stack class to hold characters of the words being entered in the text editor.

--- main.cpp
+++ main.cpp
@@ -4,13 +4,90 @@
 #include <ncurses.h>
 using namespace std;
 
-int main() {
+class StackNode
+{
+public:
+    char data;
+    StackNode *next;
+
+    StackNode(char data = 0)
+    {
+        this->data = data;
+        this->next = NULL;
+    }
+
+    ~StackNode()
+    {
+        next = NULL;
+    }
+};
+
+//Stack to hold characters of words inserted by user which form words
+class Stack
+{
+public:
+    StackNode *top;
+
+    Stack()
+    {
+        top = NULL;
+    }
+
+    ~Stack()
+    {
+        while (top != NULL)
+        {
+            StackNode *temp = top;
+            top = top->next;
+            delete temp;
+        }
+    }
+
+    void push(char data)
+    {
+        StackNode *newNode = new StackNode(data);
+        newNode->next = top;
+        top = newNode;
+    }
+
+    char pop()
+    {
+        if (top == NULL)
+        {
+            return 0;
+        }
+        StackNode *temp = top;
+        top = top->next;
+        char data = temp->data;
+        delete temp;
+        return data;
+    }
+
+    char peek()
+    {
+        if (top == NULL)
+        {
+            return 0;
+        }
+        return top->data;
+    }
+
+    bool isEmpty()
+    {
+        return top == NULL;
+    }
+
+};
+
+int main()
+{
     initscr();
     noecho();
     cbreak();
     keypad(stdscr, TRUE);
     int ch;
-    while ((ch = getch()) != 'q') {
+    while ((ch = getch()) != 'q')
+    {
         printw("Key: %c\n", ch);
     }
     endwin();
